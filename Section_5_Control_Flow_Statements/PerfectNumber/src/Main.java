6a8ae6b17eaeaad0610228f5217a0fe7cf0e8483
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-06-21 22:25:27 -0400

    Solution to perfect number challenge

---
+++ Section_5_Control_Flow_Statements/PerfectNumber/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int divisor = 1; divisor < number; divisor++) {
+            if (number % divisor == 0) {
+                sum += divisor;
+            }
+        }
+        if (sum == number) {
+            return true;
+        }
+        return false;
+    }
+}
