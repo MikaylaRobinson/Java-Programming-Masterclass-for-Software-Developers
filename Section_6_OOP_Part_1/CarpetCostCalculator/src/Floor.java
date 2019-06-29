65a8e0ec14f6e0c793b40cd9e6e662b5a5567983
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-06-28 21:51:31 -0400

    Solution to carpet cost calculator challenge problem

---
+++ Section_6_OOP_Part_1/CarpetCostCalculator/src/Floor.java
@@ -0,0 +1,24 @@
+public class Floor {
+
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+
+        if (width < 0) {
+            width = 0;
+            return;
+        }
+
+        if (length < 0) {
+            length = 0;
+            return;
+        }
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
