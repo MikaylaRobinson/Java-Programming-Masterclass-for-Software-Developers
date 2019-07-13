3a40c0fbe8c08f3ba54e24cf1bfaa3e156cc3394
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-07-12 22:39:45 -0400

    Cleaning up spacing

--- Section_11_Naming_Packages_and_Keywords/AccessModifiers/src/Account.java
+++ Section_11_Naming_Packages_and_Keywords/AccessModifiers/src/Account.java
@@ -15,7 +15,7 @@ public class Account {
     }
 
     public void deposit(int amount) {
-        if(amount > 0) {
+        if (amount > 0) {
             transactions.add(amount);
             this.balance += amount;
             System.out.println(amount + " deposited. Balance is now " + this.balance);
@@ -26,7 +26,7 @@ public class Account {
 
     public void withdraw(int amount) {
         int withdrawal = -amount;
-        if(withdrawal < 0) {
+        if (withdrawal < 0) {
             this.transactions.add(withdrawal);
             this.balance += withdrawal;
             System.out.println(amount + " withdrawn. Balance is now " + this.balance);
@@ -37,7 +37,7 @@ public class Account {
 
     public void calculateBalance() {
         this.balance = 0;
-        for(int i : this.transactions) {
+        for (int i : this.transactions) {
             this.balance += i;
         }
         System.out.println("Calculated balance is " + this.balance);
