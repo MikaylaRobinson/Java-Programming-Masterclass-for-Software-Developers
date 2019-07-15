be0ff84846366dba9dc44196081f26ff793fbe2c
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-07-14 21:20:12 -0400

    Adding exits to the adventur game example

--- Section_12_Java_Collections/AdventureGame/src/Main.java
+++ Section_12_Java_Collections/AdventureGame/src/Main.java
@@ -16,16 +16,39 @@ public class Main {
         locations.put(4, new Location(4, "stream"));
         locations.put(5, new Location(5, "forest"));
 
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 2);
+
+        locations.get(4).addExit("N", 5);
+        locations.get(4).addExit("W", 2);
+
+        locations.get(5).addExit("S", 4);
+        locations.get(5).addExit("W", 2);
+
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
             if (loc == 0) {
                 break;
             }
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for (String exit: exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
 
-            loc = scanner.nextInt();
-            if (!locations.containsKey(loc)) {
-                System.out.println("You cannot go in that direction");
+            String direction = scanner.nextLine().toUpperCase();
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go that direction");
             }
         }
     }
