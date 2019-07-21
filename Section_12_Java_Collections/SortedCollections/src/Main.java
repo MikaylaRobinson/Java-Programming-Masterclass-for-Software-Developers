8c04fefec8a3407725a8a2290e4a63b420e40018
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-07-20 23:08:52 -0400

    Shopping cart example for maps

---
+++ Section_12_Java_Collections/SortedCollections/src/Main.java
@@ -0,0 +1,76 @@
+import java.util.Map;
+
+public class Main {
+    private static StockList stockList = new StockList();
+
+    public static void main(String[] args) {
+	    StockItem temp = new StockItem("bread", 0.86, 100);
+	    stockList.addStock(temp);
+
+	    temp = new StockItem("cake", 1.1, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("car", 12.5, 2);
+        stockList.addStock(temp);
+
+        temp = new StockItem("chair", 62, 10);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.5, 200);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.45, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("juice", 2.5, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("towel", 2.4, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("vase", 8.76, 40);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+
+        Basket mikaylasBasket = new Basket("Mikayla");
+        sellItem(mikaylasBasket, "car", 1);
+        System.out.println(mikaylasBasket);
+
+        sellItem(mikaylasBasket, "car", 1);
+        System.out.println(mikaylasBasket);
+
+        sellItem(mikaylasBasket, "car", 1);
+        sellItem(mikaylasBasket, "spam", 1);
+        System.out.println(mikaylasBasket);
+
+        sellItem(mikaylasBasket, "juice", 4);
+        sellItem(mikaylasBasket, "cup", 12);
+        sellItem(mikaylasBasket, "bread", 1);
+        System.out.println(mikaylasBasket);
+        System.out.println(stockList);
+        for (Map.Entry<String, Double> price : stockList.PriceList().entrySet()) {
+            System.out.println(price.getKey() + " costs " + price.getValue());
+        }
+
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        StockItem stockItem = stockList.get(item);
+        if (stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if (stockList.sellStock(item, quantity) != 0) {
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
+    }
+}
