cdf076f819051c9bacc1d75ea486cdcba5c5fa0f
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-08-15 21:29:38 -0400

    Adding comment clarifying project structure

--- Section_11_Naming_Packages_and_Keywords/AccessModifiers/src/Accessible.java
+++ Section_11_Naming_Packages_and_Keywords/AccessModifiers/src/Accessible.java
@@ -3,7 +3,7 @@ public interface Accessible {
     // Without specifying an access level, it defaults to being accessible only within the package
     int SOME_CONSTANT = 100;
 
-    // Public access can be accessed everywhere. 
+    // Public access can be accessed everywhere.
     public void methodA();
     void methodB();
     boolean methodC();
--- Section_11_Naming_Packages_and_Keywords/TestingChallenge/src/org/challenge/series/Main.java
+++ Section_11_Naming_Packages_and_Keywords/TestingChallenge/src/org/challenge/series/Main.java
@@ -2,6 +2,7 @@ package org.challenge.series;
 
 public class Main {
 
+    // Testing the code found in ChallengeSeries Project in this package
     public static void main(String[] args) {
         for (int i = 0; i <=10; i++) {
             System.out.println(Series.nSum(i));
