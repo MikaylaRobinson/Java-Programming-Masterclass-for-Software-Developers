dd9eff8e2be4423570e3b5efdb05253ecc109db5
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-08-15 21:26:08 -0400

    Adding comments about the access modifiers used

--- Section_11_Naming_Packages_and_Keywords/AccessModifiers/src/Accessible.java
+++ Section_11_Naming_Packages_and_Keywords/AccessModifiers/src/Accessible.java
@@ -1,5 +1,9 @@
 public interface Accessible {
+
+    // Without specifying an access level, it defaults to being accessible only within the package
     int SOME_CONSTANT = 100;
+
+    // Public access can be accessed everywhere. 
     public void methodA();
     void methodB();
     boolean methodC();
