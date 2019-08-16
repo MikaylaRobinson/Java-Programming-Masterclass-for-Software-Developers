11769f52af6be28d90ddb6641c96afaacc16445f
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-08-15 22:13:52 -0400

    Adding comment about use of interfaces

--- Section_9_Inner_and_Abstract_Classes_Interfaces/InterfaceExample/src/ISaveable.java
+++ Section_9_Inner_and_Abstract_Classes_Interfaces/InterfaceExample/src/ISaveable.java
@@ -1,5 +1,8 @@
 import java.util.List;
 
+// Interfaces are used to introduce a list of possible methods to be used by the class that implements it.
+// The classes then overwrite the abstract methods given in the interface as seen in the Monster and Player classes.
+// They both need the read and write methods, but they are able to be slightly different in each class.
 public interface ISaveable {
     List<String> write();
     void read(List<String> savedValues);
