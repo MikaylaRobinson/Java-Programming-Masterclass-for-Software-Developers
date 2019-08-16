380fd504209a70b760c86f6d604ca92d1e90a889
Mikayla Robinson <mikayla22robinson@gmail.com>
2019-08-16 12:22:02 -0400

    Adding comment about composition example

--- Section_7_OOP_Part_2/CompositionChallengeExercise/src/Kitchen.java
+++ Section_7_OOP_Part_2/CompositionChallengeExercise/src/Kitchen.java
@@ -1,5 +1,8 @@
 public class Kitchen {
 
+    // This is an example of composition with each object below making up the
+    // kitchen. Objects of other classes I created are used as the fields for the
+    // larger idea of a kitchen class. 
     private Oven theOven;
     private Fridge theFridge;
     private Cabinet theCabinet;
